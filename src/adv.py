078cc1a555345f08117486557352d2badd2babd8
Lord-Kanzler <climberalex@gmail.com>
2020-06-05 11:14:18 -0400

    MVP2 and edits

--- src/adv.py
+++ src/adv.py
@@ -2,28 +2,36 @@
 from room import Room
 from player import Player
 from item import Item
-import csv
 import textwrap
+import csv
+
 
 # Declare all the rooms
 
 room = {
-    'outside':  Room("Outside Cave Entrance",
-                     "North of you, the cave mount beckons"),
-
-    'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
-passages run north and east."""),
-
-    'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
+    "outside": Room("Outside Cave Entrance", "North of you, the cave mount beckons"),
+    "foyer": Room(
+        "Foyer",
+        """Dim light filters in from the south. Dusty
+passages run north and east.""",
+    ),
+    "overlook": Room(
+        "Grand Overlook",
+        """A steep cliff appears before you, falling
 into the darkness. Ahead to the north, a light flickers in
-the distance, but there is no way across the chasm."""),
-
-    'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
-to north. The smell of gold permeates the air."""),
-
-    'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
+the distance, but there is no way across the chasm.""",
+    ),
+    "narrow": Room(
+        "Narrow Passage",
+        """The narrow passage bends here from west
+to north. The smell of gold permeates the air.""",
+    ),
+    "treasure": Room(
+        "Treasure Chamber",
+        """You've found the long-lost treasure
 chamber! Sadly, it has already been completely emptied by
-earlier adventurers. The only exit is to the south."""),
+earlier adventurers. The only exit is to the south.""",
+    ),
 }
 
 # Using textwrap for for better readability
@@ -31,22 +39,44 @@ wrapper = textwrap.TextWrapper(width=70)
 
 # Link rooms together
 
-room['outside'].n_to = room['foyer']
-room['foyer'].s_to = room['outside']
-room['foyer'].n_to = room['overlook']
-room['foyer'].e_to = room['narrow']
-room['overlook'].s_to = room['foyer']
-room['narrow'].w_to = room['foyer']
-room['narrow'].n_to = room['treasure']
-room['treasure'].s_to = room['narrow']
+room["outside"].n_to = room["foyer"]
+room["foyer"].s_to = room["outside"]
+room["foyer"].n_to = room["overlook"]
+room["foyer"].e_to = room["narrow"]
+room["overlook"].s_to = room["foyer"]
+room["narrow"].w_to = room["foyer"]
+room["narrow"].n_to = room["treasure"]
+room["treasure"].s_to = room["narrow"]
 
 #
 # Main
 #
 
+# Takes list of item names and returns a list of the associated ids
+def name_to_id(names):
+    split = names.split(",")
+    base = ""
+    for item in split:
+        base = base + "'" + item.replace(" ", "").lower() + "',"
+    return eval("[" + base + "]")
+
+
+# Define items from the item_src list
+item_list = {}
+
+with open("src/item_list.txt", "r") as f:
+    for line in f:
+        data = eval(line)
+        item_list[data["id"]] = Item(data["name"], data["description"])
+
+# Establish items in rooms
+foyer_items = [item_list["rustysword"], item_list["littlefigurine"]]
+room["foyer"].add_items(foyer_items)
+
+
 # Make a new player object that is currently in the 'outside' room.
-player_name = input('Enter a name for your character: ')
-player1 = Player(player_name, room['outside'])
+player_name = input("How would you like to call your character?: ")
+player1 = Player(player_name, room["outside"])
 
 # Write a loop that:
 #
@@ -59,35 +89,62 @@ player1 = Player(player_name, room['outside'])
 #
 # If the user enters "q", quit the game.
 
-while(True):
+while True:
     # Determine current room
     current_room = player1.current_room
-    
+
     # Print buffer spaces to improve readability
-    print('')
-    print('-'*70)
-    
+    print("")
+    print("-" * 70)
+
     # Print current Location and location description/items
-    print('Current Room: {}'.format(current_room.name))
+    print("Current Room: {}".format(current_room.name))
     [print(line) for line in wrapper.wrap(text=current_room.description)]
+    item_str = "Items in room:\t{}".format(current_room.check_items())
+    [print(line) for line in wrapper.wrap(text=item_str)]
 
     # Input from user
-    inp = input('Decide what you want to do? ')
+    inp = input(
+        "Decide what you want to do? [Options: Directions:'n', 'e', 's', 'w'  Actions: 'take', 'grab', 'get']"
+    )
 
     # Print buffer to make it easier to read
-    print('-'*70)
+    print("-" * 70)
 
     # Preprocess input
     inputs = inp.split(maxsplit=1)
 
     # Process Input
-    if(len(inputs) == 1):
+    if len(inputs) == 1:
         inp = inputs[0]
-        if(inp in ['q', 'quit', 'exit']):
+        if inp in ["q", "quit", "exit"]:
             break
-        
-        elif(inp in ['n', 'e', 's', 'w']):
+
+        elif inp in ["n", "e", "s", "w"]:
             player1.move_player(inp)
-            
+
+        if inp in ["i", "inventory"]:
+            print("Inventory:")
+            [print(line) for line in player1.get_items()]
+
+    elif len(inputs) == 2:
+        verb, obj = inputs[0], inputs[1]
+        if verb in ["take", "grab", "get"]:
+            # Generate list of target item's id's from input
+            targ_items = name_to_id(obj)
+            # Attempt to remove target items from room
+            removed_items_ids, removed_items = current_room.remove_items(targ_items)
+            # Add items to player inventory
+            player1.add_items(removed_items)
+
+        if verb in ["remove", "drop", "toss"]:
+            # Generate list of target item's id's from input
+            targ_items = name_to_id(obj)
+            # Attempt to remove target items from room
+            removed_item_ids, removed_items = player1.remove_items(targ_items)
+            # Add items to room
+            items = {id: item for id, item in zip(removed_item_ids, removed_items)}
+            current_room.add_items(removed_items)
+
     else:
-        print('Invalid Input')
+        print("Invalid Input")
--- src/room.py
+++ src/room.py
@@ -3,31 +3,91 @@
 
 
 class Room:
-	def __init__(self, name, description, items=None):
-	self.name = name
-	self.description = description
-	if(items is None):
-        self.items = {}
-    else:
-        self.items = {item.id:item for item in items}
+    def __init__(self, name, description, items=None):
+        self.name = name
+        self.description = description
+        if items is None:
+            self.items = {}
+        else:
+            self.items = {item.id: item for item in items}
 
-    #Available rooms to direction
-	self.n_to = None
-	self.s_to = None
-	self.e_to = None
-	self.w_to = None
+        # Available rooms to direction
+        self.n_to = None
+        self.s_to = None
+        self.e_to = None
+        self.w_to = None
 
-	# def __str__(self):
-	# output = ""
-	# output += f"Room: {self.name}\n"
-	# output += f"Description: {self.description}"
+    # def __str__(self):
+    # output = ""
+    # output += f"Room: {self.name}\n"
+    # output += f"Description: {self.description}"
 
-	# if len(self.items) > 0:
-	# for index, item in enumerate(self.items):
-	# output += "\nRoom Item: "+str(item)
+    # if len(self.items) > 0:
+    # for index, item in enumerate(self.items):
+    # output += "\nRoom Item: "+str(item)
 
-	# return output
+    # return output
 
-    # Accessor method to return  key/value pairs of the items dict
+    # Accessor method to return key/value pairs of the items dict
     def view_items(self):
         return items.items()
+
+    # Function to create a printable string of all current items in the room
+    def check_items(self):
+        if self.items is None:
+            return "This room is empty"
+        else:
+            base = ""
+            for item in self.items:
+                base = base + self.items[item].name + ", "
+            return base[:-2]
+
+    # Add item
+    def add_items(self, item_list):
+        # Check to make sure type of object passed is a list
+        if type(item_list) != list:
+            raise TypeError("Expected a list")
+
+        # Add each item in list to the items dict
+        for item in item_list:
+            self.items[item.id] = item
+
+    # Remove Item
+    def remove_items(self, item_id_list):
+        # Check if type of object passed is a list
+        if type(item_id_list) != list:
+            raise TypeError("Expected a list")
+
+        # Check to make sure type of object passed is a list
+        if type(item_id_list) != list:
+            raise TypeError("Expected a list")
+
+        # Check if any of the passed item id's are in the room's item dict
+        if any([id in self.items for id in item_id_list]):
+            # Create a list to represent the result of trying to remove items
+            # from the items dictionary
+            status = []
+            removed_items = []
+
+            # Attempt to remove all items passed to this method in the list
+            for id in item_id_list:
+                try:
+                    # Attempt to delete key
+                    item = self.items.pop(id)
+                    removed_items.append(item)
+
+                    # If successful, the item will be deleted and append True to status
+                    status.append(True)
+
+                except KeyError as ke:
+                    # If key is not found in items, append a False to status
+                    status.append(False)
+
+            # Return which items from list successfully were removed from items dict
+            removed_item_ids = [
+                item for item, removed in zip(item_id_list, status) if removed
+            ]
+
+            return (removed_item_ids, removed_items)
+        else:
+            return []
